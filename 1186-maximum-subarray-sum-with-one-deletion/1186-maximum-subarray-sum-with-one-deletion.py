26bbd0504d9efd6ba440b2037a40f7e763ac363f
Arif Basha <arifbasha559@gmail.com>
2026-07-04 18:20:39 +0530

    Time: 19 ms (70.14%), Space: 28 MB (31.76%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.py
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.py
@@ -5,11 +5,10 @@ class Solution:
         res = a[0]
 
         for i in range(1, len(a)):
-            prevNoDel = noDel
 
+
+            oneDel = max(noDel, oneDel + a[i])
             noDel = max(a[i], noDel + a[i])
-
-            oneDel = max(prevNoDel, oneDel + a[i])
 
             res = max(res, noDel, oneDel)
 
